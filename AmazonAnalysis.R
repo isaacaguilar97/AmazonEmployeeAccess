f8d78dc0a455c4686205d5fba7d86ddd082f9201
Isaac Aguilar <oscarisaacaguilarembila@gmail.com>
2023-10-23 13:18:56 -0600

    naive bayes pca

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -218,112 +218,53 @@ amazon_test <- vroom('./test.csv')
 # 
 # 
 # # Naive Bayes -------------------------------------------------------------
-# 
-# 
-# ## nb model3
-# nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
-#   set_mode("classification") %>%
-#   set_engine("naivebayes") # install discrim library for the naivebayes eng
-# 
-# my_recipe <- recipe(ACTION~., data=amazon_train) %>%
-#   step_mutate_at(all_numeric_predictors(), fn = factor)  %>% # turn all numeric features into factors
-#   step_other(all_nominal_predictors(), threshold = .01) %>% # combines categorical values that occur <5% into an "other" value
-#   step_dummy(all_nominal_predictors())
-# 
-# nb_wf <- workflow() %>%
-#   add_recipe(my_recipe) %>%
-#   add_model(nb_model)
-# 
-# ## Tune smoothness and Laplace here
-# tuning_grid <- grid_regular(smoothness(),
-#                             Laplace(),
-#                             levels = 5)
-# 
-# ## Set up K-fold CV
-# folds <- vfold_cv(amazon_train, v = 10, repeats=1)
-# 
-# CV_results <- nb_wf %>%
-#   tune_grid(resamples=folds,
-#             grid=tuning_grid,
-#             metrics=metric_set(MSE))
-# 
-# ## Find best tuning parameters
-# bestTune <- CV_results %>% 
-#   select_best("roc_auc")
-# 
-# ## Finalize workflow and predict
-# 
-# final_wf <- nb_wf %>% 
-#   finalize_workflow(bestTune) %>% 
-#   fit(data=amazon_train)
-# 
-# ## Predict
-# #predict(nb_wf, new_data=myNewData, type=)
-# 
-# amazon_predictions <- final_wf %>%
-#   predict(new_data = amazon_test, type = "prob")
-# 
-# #save(file="./MyFile.RData", list=c("amazon_predictions", "final_wf", "bestTune", "CV_results"))
-# 
-# # Format table
-# amazon_test$Action <- amazon_predictions$.pred_1
-# results <- amazon_test %>%
-#   rename(Id = id) %>%
-#   select(Id, Action)
-# 
-# 
-# # get csv file
-# vroom_write(results, 'AmazonPredspreg.csv', delim = ",")
 
 
-# KNN ---------------------------------------------------------------------
-
-
-## knn model
-knn_model <- nearest_neighbor(neighbors=tune()) %>% # set or tune
+## nb model3
+nb_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
   set_mode("classification") %>%
-  set_engine("kknn")
+  set_engine("naivebayes") # install discrim library for the naivebayes eng
 
-## Reciepe
 my_recipe <- recipe(ACTION~., data=amazon_train) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor)  %>% # turn all numeric features into factors
   step_other(all_nominal_predictors(), threshold = .01) %>% # combines categorical values that occur <5% into an "other" value
   step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
-  step_normalize(all_numeric_predictors()) %>%
-  step_pca(all_predictors(), threshold=.8) #Threshold is between 0 and 1
+  step_pca(all_predictors(), threshold=.9) #Threshold is between 0 and 1
 
-## Workflow
-knn_wf <- workflow() %>%
+nb_wf <- workflow() %>%
   add_recipe(my_recipe) %>%
-  add_model(knn_model)
+  add_model(nb_model)
 
-## Tune
-tuning_grid <- grid_regular(neighbors(),
-                            levels = 10)
+## Tune smoothness and Laplace here
+tuning_grid <- grid_regular(smoothness(),
+                            Laplace(),
+                            levels = 5)
 
 ## Set up K-fold CV
 folds <- vfold_cv(amazon_train, v = 10, repeats=1)
 
-CV_results <- knn_wf %>%
+CV_results <- nb_wf %>%
   tune_grid(resamples=folds,
             grid=tuning_grid,
-            metrics=metric_set(roc_auc))
+            metrics=metric_set(MSE))
 
 ## Find best tuning parameters
-bestTune <- CV_results %>% 
+bestTune <- CV_results %>%
   select_best("roc_auc")
 
 ## Finalize workflow and predict
 
-final_wf <- knn_wf %>% 
-  finalize_workflow(bestTune) %>% 
+final_wf <- nb_wf %>%
+  finalize_workflow(bestTune) %>%
   fit(data=amazon_train)
 
-## Fit or Tune Model HERE
+## Predict
+#predict(nb_wf, new_data=myNewData, type=)
+
 amazon_predictions <- final_wf %>%
   predict(new_data = amazon_test, type = "prob")
 
-save(file="./MyFile.RData", list=c("amazon_predictions", "final_wf", "bestTune", "CV_results"))
+#save(file="./MyFile.RData", list=c("amazon_predictions", "final_wf", "bestTune", "CV_results"))
 
 # Format table
 amazon_test$Action <- amazon_predictions$.pred_1
@@ -331,7 +272,67 @@ results <- amazon_test %>%
   rename(Id = id) %>%
   select(Id, Action)
 
+
+# get csv file
 vroom_write(results, 'AmazonPredspreg.csv', delim = ",")
+
+
+# KNN ---------------------------------------------------------------------
+
+
+## knn model
+# knn_model <- nearest_neighbor(neighbors=tune()) %>% # set or tune
+#   set_mode("classification") %>%
+#   set_engine("kknn")
+# 
+# ## Reciepe
+# my_recipe <- recipe(ACTION~., data=amazon_train) %>%
+#   step_mutate_at(all_numeric_predictors(), fn = factor)  %>% # turn all numeric features into factors
+#   step_other(all_nominal_predictors(), threshold = .01) %>% # combines categorical values that occur <5% into an "other" value
+#   step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
+#   step_normalize(all_numeric_predictors()) %>%
+#   step_pca(all_predictors(), threshold=.9) #Threshold is between 0 and 1
+# 
+# ## Workflow
+# knn_wf <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(knn_model)
+# 
+# ## Tune
+# tuning_grid <- grid_regular(neighbors(),
+#                             levels = 10)
+# 
+# ## Set up K-fold CV
+# folds <- vfold_cv(amazon_train, v = 10, repeats=1)
+# 
+# CV_results <- knn_wf %>%
+#   tune_grid(resamples=folds,
+#             grid=tuning_grid,
+#             metrics=metric_set(roc_auc))
+# 
+# ## Find best tuning parameters
+# bestTune <- CV_results %>% 
+#   select_best("roc_auc")
+# 
+# ## Finalize workflow and predict
+# 
+# final_wf <- knn_wf %>% 
+#   finalize_workflow(bestTune) %>% 
+#   fit(data=amazon_train)
+# 
+# ## Fit or Tune Model HERE
+# amazon_predictions <- final_wf %>%
+#   predict(new_data = amazon_test, type = "prob")
+# 
+# save(file="./MyFile.RData", list=c("amazon_predictions", "final_wf", "bestTune", "CV_results"))
+# 
+# # Format table
+# amazon_test$Action <- amazon_predictions$.pred_1
+# results <- amazon_test %>%
+#   rename(Id = id) %>%
+#   select(Id, Action)
+# 
+# vroom_write(results, 'AmazonPredspreg.csv', delim = ",")
 
 
 
